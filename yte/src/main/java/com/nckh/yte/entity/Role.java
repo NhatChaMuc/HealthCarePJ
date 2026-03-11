e156675f69dde69538fc000116e3d8488b69358f
NhatChaMuc <nguynlenhat006@gmail.com>
2026-03-11 22:10:41 +0700

    Update Role.java

--- yte/src/main/java/com/nckh/yte/entity/Role.java
+++ yte/src/main/java/com/nckh/yte/entity/Role.java
@@ -2,20 +2,18 @@ package com.nckh.yte.entity;
 
 import jakarta.persistence.*;
 import lombok.*;
+
 import java.util.UUID;
 
-@Entity
-@Table(name = "roles")
-@Getter
-@Setter
+@Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
 public class Role {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
+    @GeneratedValue
     private UUID id;
 
-    @Column(unique = true, nullable = false)
     private String name;
 }
