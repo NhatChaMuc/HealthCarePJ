f2c9d0c47ea0c036ff288cccf99bbedb430212e6
NhatChaMuc <nguynlenhat006@gmail.com>
2026-03-11 22:40:44 +0700

    Update GeminiRequest.java

--- yte/src/main/java/com/nckh/yte/gemini/GeminiRequest.java
+++ yte/src/main/java/com/nckh/yte/gemini/GeminiRequest.java
@@ -1,24 +1,33 @@
-// Đặt file này trong: src/main/java/com/nckh/yte/gemini/GeminiRequest.java
 package com.nckh.yte.gemini;
 
-import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 public class GeminiRequest {
+
     private List<Content> contents;
 
     @Data
+    @NoArgsConstructor
     @AllArgsConstructor
     public static class Content {
+
         private List<Part> parts;
+
     }
 
     @Data
+    @NoArgsConstructor
     @AllArgsConstructor
     public static class Part {
+
         private String text;
+
     }
 }
