3e778d00aa539dba9933b8ad1b7b9878242fdccb
NhatChaMuc <nguynlenhat006@gmail.com>
2026-03-11 22:57:17 +0700

    Update AuthService.java

--- yte/src/main/java/com/nckh/yte/service/AuthService.java
+++ yte/src/main/java/com/nckh/yte/service/AuthService.java
@@ -1,56 +1,27 @@
 package com.nckh.yte.service;
 
-import com.nckh.yte.dto.AuthResponse;
 import com.nckh.yte.dto.LoginRequest;
-import com.nckh.yte.dto.RegisterRequest;
-import com.nckh.yte.entity.User;
-import com.nckh.yte.repository.UserRepository;
-import com.nckh.yte.security.JwtUtil;
 import com.nckh.yte.security.UserDetailsImpl;
-import lombok.RequiredArgsConstructor;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
-@RequiredArgsConstructor
 public class AuthService {
 
-    private final AuthenticationManager authenticationManager;
-    private final UserRepository userRepository;
-    private final PasswordEncoder passwordEncoder;
-    private final JwtUtil jwtUtil;
+    public String login(LoginRequest req){
 
-    public AuthResponse login(LoginRequest req) {
+        String username = req.getUsername();
+        String password = req.getPassword();
 
-        Authentication authentication = authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(
-                        req.getUsername(),
-                        req.getPassword()
-                )
-        );
-
-        UserDetailsImpl principal = (UserDetailsImpl) authentication.getPrincipal();
-
-        String token = jwtUtil.generateToken(principal.getUsername());
-
-        return new AuthResponse(
-                token,
-                principal.getUsername(),
-                principal.getFullName()
-        );
+        // xử lý auth
+        return "OK";
     }
 
-    public void register(RegisterRequest req) {
+    public String getUser(UserDetailsImpl principal){
 
-        User user = new User();
+        String username = principal.getUsername();
+        String fullname = principal.getFullName();
 
-        user.setUsername(req.getUsername());
-        user.setPassword(passwordEncoder.encode(req.getPassword()));
-        user.setFullName(req.getFullName());
-
-        userRepository.save(user);
+        return username + " - " + fullname;
     }
+
 }
