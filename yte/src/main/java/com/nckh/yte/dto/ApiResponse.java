1b7278c9540805607a0dc87366bd40fc3d57c847
NhatChaMuc <nguynlenhat006@gmail.com>
2026-03-12 00:17:26 +0700

    Update ApiResponse.java

--- yte/src/main/java/com/nckh/yte/dto/ApiResponse.java
+++ yte/src/main/java/com/nckh/yte/dto/ApiResponse.java
@@ -1,12 +1,35 @@
 package com.nckh.yte.dto;
 
-import lombok.*;
-
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class ApiResponse<T> {
 
     private String message;
     private T data;
+
+    // Constructor mặc định
+    public ApiResponse() {
+    }
+
+    // Constructor với message và data
+    public ApiResponse(String message, T data) {
+        this.message = message;
+        this.data = data;
+    }
+
+    // Getter và Setter cho message
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    // Getter và Setter cho data
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
 }
