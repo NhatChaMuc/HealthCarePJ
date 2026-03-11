e4038a7bbe76cf0d3d17d97e705ccad0a8718d04
NhatChaMuc <nguynlenhat006@gmail.com>
2026-03-11 22:39:53 +0700

    Update AuthService.java

--- yte/src/main/java/com/nckh/yte/service/AuthService.java
+++ yte/src/main/java/com/nckh/yte/service/AuthService.java
@@ -1,29 +1,56 @@
 package com.nckh.yte.service;
 
-import com.nckh.yte.dto.*;
+import com.nckh.yte.dto.AuthResponse;
+import com.nckh.yte.dto.LoginRequest;
+import com.nckh.yte.dto.RegisterRequest;
+import com.nckh.yte.entity.User;
+import com.nckh.yte.repository.UserRepository;
+import com.nckh.yte.security.JwtUtil;
 import com.nckh.yte.security.UserDetailsImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class AuthService {
 
-    public AuthResponse login(UserDetailsImpl principal, String token){
+    private final AuthenticationManager authenticationManager;
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtUtil jwtUtil;
+
+    public AuthResponse login(LoginRequest req) {
+
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        req.getUsername(),
+                        req.getPassword()
+                )
+        );
+
+        UserDetailsImpl principal = (UserDetailsImpl) authentication.getPrincipal();
+
+        String token = jwtUtil.generateToken(principal.getUsername());
 
         return new AuthResponse(
                 token,
                 principal.getUsername(),
                 principal.getFullName()
         );
-
     }
 
-    public void register(RegisterRequest req){
+    public void register(RegisterRequest req) {
 
-        String username = req.getUsername();
-        String password = req.getPassword();
-        String fullName = req.getFullName();
+        User user = new User();
 
-        // xử lý lưu DB
+        user.setUsername(req.getUsername());
+        user.setPassword(passwordEncoder.encode(req.getPassword()));
+        user.setFullName(req.getFullName());
+
+        userRepository.save(user);
     }
-
 }
