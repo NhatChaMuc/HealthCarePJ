ceb323e0e637b2bf4009fc82126cfad903eea6e7
NhatChaMuc <nguynlenhat006@gmail.com>
2026-03-11 22:41:03 +0700

    Update DrugApiConfig.java

--- yte/src/main/java/com/nckh/yte/DrugApiConfig.java
+++ yte/src/main/java/com/nckh/yte/DrugApiConfig.java
@@ -1,20 +1,15 @@
 package com.nckh.yte;
 
-import lombok.Data;
-import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 
-@Data
 @Configuration
-@ConfigurationProperties(prefix = "ai.gemini")
 public class DrugApiConfig {
 
-    private String model;
-    private String apiKey;
-    private String baseUrl;
-
-    public String buildUrl() {
-        return baseUrl + model + ":generateContent?key=" + apiKey;
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 
 }
